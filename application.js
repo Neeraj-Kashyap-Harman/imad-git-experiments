e98abf3e9a5ea041dbc33dd00d6c942340b2c9c2
Neeraj-Kashyap <neeraj1610007@gmail.com>
2018-03-28 22:28:49 +0530

    Finally done with this code and new feature.

--- application.js
+++ application.js
@@ -1,6 +1,8 @@
 function aBigNewFeature(){
 	console.log('Writing a lot of code');
 	console.log('Halway done');
+	console.log('Completely done with this new aBigNewFeature');
+	
 }
 function something()
 {
