758271f096fea4ee28e356bf7baf8673252db083
Neeraj-Kashyap <neeraj1610007@gmail.com>
2018-03-28 22:15:45 +0530

    Halway done with this code in github

--- application.js
+++ application.js
@@ -1,6 +1,11 @@
+function aBigNewFeature(){
+	console.log('Writing a lot of code');
+	console.log('Halway done');
+}
 function something()
 {
 	console.log('I\'m something');
 }
 console.log("Hello World");
 something();
+aBigNewFeature();
